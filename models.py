5e7681303d66b93b4c28f7b7e5b8f5f08e44ba1d
SetorSe7e <112187814+SetorSe7e@users.noreply.github.com>
2026-01-29 11:05:35 -0300

    Password 225

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(225), nullable=False)
     role = db.Column(db.String(50), nullable=False) # director, teacher, guardian
 
 class Guardian(db.Model):
