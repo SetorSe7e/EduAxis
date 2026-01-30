08f380f9cb13d2637e9057d861fcb4a840f9f591
SetorSe7e <112187814+SetorSe7e@users.noreply.github.com>
2026-01-30 10:45:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from sqlalchemy import extract
+from sqlalchemy import func, extract
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
